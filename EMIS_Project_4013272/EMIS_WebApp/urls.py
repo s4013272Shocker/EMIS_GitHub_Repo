6857e99ffc178a324454f574c448ba917132a3e5
s4013272Shocker <s4013272@lsbu.ac.uk>
2024-07-15 13:03:49 +0100

    Exam Dynamic Template page Setup
    index.html file has URL links refined. MySQL database is required for dynamic data to be active; MySQL database not yet set up.

--- EMIS_Project_4013272/EMIS_WebApp/urls.py
+++ EMIS_Project_4013272/EMIS_WebApp/urls.py
@@ -2,5 +2,6 @@ from EMIS_WebApp import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.index, name ="index")
+    path("", views.index, name ="index"),
+    path ("exam", views.exam, name = "exam")
 ]
--- EMIS_Project_4013272/EMIS_WebApp/views.py
+++ EMIS_Project_4013272/EMIS_WebApp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render (request, "index.html")
+
+def exam(request):
+    return render (request, "exam.html")
